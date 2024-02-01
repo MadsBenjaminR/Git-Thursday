e3e5463bc88f6525f0bf3fbd5410e45d5b5ffdd8
Jeppe Koch <cph-jk469@cphbusiness.dk>
2024-02-01 12:11:08 +0100

    Added a main method that prints through 1-100

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -1,6 +1,16 @@
 public class main {
 
     public static void main(String[] args) {
-        System.out.println("VIRKE NU!");
+        for(int i = 1; i<=100; i++) {
+            if (i % 3 == 0 && i % 5 == 0) {
+                System.out.println("FizzBizz");
+            } else if (i % 3 == 0) {
+                System.out.println("Fizz");
+            } else if (i % 5 == 0) {
+                System.out.println("Bizz");
+            } else {
+                System.out.println(i);
+            }
+        }
     }
 }
