846508d42e681755f6febfd34e11aecaf209bec4
Mads Benjamin Ribberholt <benjamin.ribberholt@gmail.com>
2024-02-01 12:44:25 +0100

    forsøg på første merge

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -1,6 +1,15 @@
 public class main {
 
     public static void main(String[] args) {
-        System.out.println("VIRKE NU!");
+        for (int i =1;i<=200;i++){
+            if (i% 3==0 && i %5==0){
+                System.out.println("BuzzFish");
+            } else if (i%3==0) {
+                System.out.println("Buzz ");
+            } else if (i%5==0) {
+                System.out.println("Fish");
+
+            }
+        }
     }
 }
